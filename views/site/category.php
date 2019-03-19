9c470f033e2cf081207264cff9910d53013f498c
Harser <harser@xaker.ru>
2019-03-19 17:43:22 +0600

    category yes y :ew

--- views/site/category.php
+++ views/site/category.php
@@ -15,13 +15,44 @@ use app\models\Category;
     <?php $categoryes = Category::find()->all(); ?>
     <?php if($categoryes):?>
     <?php foreach ($categoryes as $category):?>
+    <!--
     <div class="card" style="width: 18rem;">
         <img class="card-img-top" src="<?=$category->imgUrl();?>" alt="">
         <div class="card-body">
             <h4 class="h4"><?=$category->name;?></h4>
             <p class="card-text"><?=$category->description;?></p>
         </div>
+    </div>-->
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="card">
+                <a href="#fake-url">
+                    <img class="card-img-top" src="<?=$category->imgUrl();?>" alt="">
+                    <div class="card-body">
+                        <h4 class="h4"><?=$category->name;?></h4>
+                        <p class="card-text"><?=$category->description;?></p>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
+    <style>
+        .card {
+            max-width: 18rem;
+            margin: 0 auto;
+        }
+        .card img {
+            object-fit: cover;
+            height: 18rem;
+        }
+        .card a {
+            text-decoration: none;
+            color: #000;
+        }
+        .card a:hover,.card a:visited, .card a:active {
+            color: #343a40;
+        }
+    </style>
     <?php endforeach;?>
     <?php endif;?>
 </section>
